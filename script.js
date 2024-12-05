0491120afd0930eb52e70b976f049ded6ba736b5
KledionBerisha <berisha.kledion15@gmail.com>
2024-12-05 16:48:47 +0100

    validimi i login form

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function validateForm(){
-    let x=document.forms["myForm"]["Username"]["Password"].value;
-    if(x==""){
-        alert("Username and password must be filled out!");
-    }
-}
+document.addEventListener("DOMContentLoaded",() =>{
+    const submit=document.getElementById("submit");
+    
+    const validateForm=()=>{
+        
+        const email=document.getElementById("email").value.trim();
+        const password=document.getElementById("password").value.trim();
+        if(email===""){
+            alert("Please fill in your email!");
+            email.focus();
+            return false;
+        }
+        if(password===""){
+            alert("Please fill in your password!");
+            password.focus();
+            return false;
+        }
+        if(!emailValid(email)){
+            alert("Please fill in a valid email!");
+            email.focus();
+            return false;
+        }
+        alert("Form submitted successfully");
+        return true;
+    };
+        const emailValid = (email) =>{
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                return emailRegex.test(email.toLowerCase());
+            };
+        submit.addEventListener('click', (event) =>{
+            if(!validateForm()){
+                event.preventDefault();
+            }
+        });
+});
